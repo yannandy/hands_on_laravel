2ae597f0f01c6698e3c3131501afc8f9e73e96a4
yannandy <aballoyannick@gmail.com>
2021-10-13 10:22:02 +0100

    /mainpage

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mainpage');
 });
 Route::get('mainpage', 'App\Http\Controllers\mainPageController@index');
 Route::get('article', 'App\Http\Controllers\mainPageController@article');
